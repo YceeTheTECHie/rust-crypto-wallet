b5d1533d0254ee905fc390193676e3d75b07f2d4
Boluwatife Ayodele <waplurd1@gmail.com>
2021-12-15 16:18:14 +0100

    established connection to a node via alchemy

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use anyhow::Result;
 mod wallets;
 
+const URL : &str = "https://eth-ropsten.alchemyapi.io/v2/IKep9GN_OVBd_6JG5LCTtN5TZ5irvpeU";
 fn main() -> Result<()> {
     let keypair = wallets::create_keypair();
     println!("{:?}", keypair);
+    let web3 = wallets::establish_connection(URL)?;
     Ok(())
 }
--- src/wallets.rs
+++ src/wallets.rs
@@ -1,8 +1,17 @@
 use secp256k1::{PublicKey,SecretKey,rand::{rngs,SeedableRng}};
 use anyhow::Result;
+use web3::{self,Web3,transports::Http};
 
 pub fn create_keypair() -> Result<(SecretKey,PublicKey)> {
     let secp = secp256k1::Secp256k1::new();
-    let mut rng = rngs::StdRng::seed_from_u64(6);
+    println!("{:?}", secp);
+    let mut rng = rngs::StdRng::seed_from_u64(8);
     Ok(secp.generate_keypair(&mut rng))
+
+}
+
+pub fn establish_connection(url : &str) -> Result<Web3<Http>> {
+    let transport = web3::transports::Http::new(url)?;
+    // let transport = web3::Transport:Http::new(url)?;
+    Ok(web3::Web3::new(transport))
 }

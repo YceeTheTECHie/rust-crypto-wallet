73aeb9f6d4bc77dca8cde5fffa7e401e804f98ee
Boluwatife Ayodele <waplurd1@gmail.com>
2021-12-17 01:38:44 +0100

    implement sign and send function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use anyhow::Result;
 mod wallets;
-
+use web3::types::{Address};
+use std::{  str::FromStr};
+use tokio;
 const URL : &str = "https://eth-ropsten.alchemyapi.io/v2/IKep9GN_OVBd_6JG5LCTtN5TZ5irvpeU";
-fn main() -> Result<()> {
-    let keypair = wallets::create_keypair();
+
+#[tokio::main]
+
+async fn main() -> Result<()> {
+    let keypair = wallets::create_keypair()?;
     println!("{:?}", keypair);
     let web3 = wallets::establish_connection(URL)?;
+    let to_address = Address::from_str("0x1b42ee3294DA8A1Fa125061674067128e2AFEB6c")?;
+    let tx_object = wallets::create_tx_object(to_address,7)?;
+    let result = wallets::sign_tx(web3.clone(),tx_object,keypair.0).await?;
+    println!("result {:?}", result);
     Ok(())
 }
--- src/wallets.rs
+++ src/wallets.rs
@@ -1,6 +1,7 @@
 use secp256k1::{PublicKey,SecretKey,rand::{rngs,SeedableRng}};
 use anyhow::Result;
-use web3::{self,Web3,transports::Http};
+use web3::{self,Web3,transports::Http, types::TransactionParameters};
+use web3::types::{U256, H256, H160};
 
 pub fn create_keypair() -> Result<(SecretKey,PublicKey)> {
     let secp = secp256k1::Secp256k1::new();
@@ -14,4 +15,17 @@ pub fn establish_connection(url : &str) -> Result<Web3<Http>> {
     let transport = web3::transports::Http::new(url)?;
     // let transport = web3::Transport:Http::new(url)?;
     Ok(web3::Web3::new(transport))
+}
+
+pub fn create_tx_object(to: H160, value : usize) ->  Result<TransactionParameters> {
+    Ok(TransactionParameters {
+        to : Some(to),
+        value : U256::exp10(value),
+        ..Default::default()
+    })
+}
+
+pub async fn sign_tx(web3: Web3<Http>, tx_object : TransactionParameters,secKey:SecretKey)-> Result<(H256)>{
+    let signed = web3.accounts().sign_transaction(tx_object,&secKey).await?;
+    Ok(web3.eth().send_raw_transaction(signed.raw_transaction).await?)
 }
